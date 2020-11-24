527167d7f8de3f7f3e349839afcde0cd7b24d1cc
Jessica Ramos Molina <jessicaramosmolina@Jessicas-Air.hsd1.ca.comcast.net>
2020-11-23 21:13:22 -0800

    update largest fire

---
+++ api_keys.py
@@ -0,0 +1,11 @@
+weather_api_key = 'f2deb80b0bea75908cebab9ed8582510'
+
+# Google API Key
+g_key = 'AIzaSyCn5p2NDEjlDDfLESzUlsRtI_gnGCNbvnQ'
+
+# OneAtlas API Key
+atlas_key = 'GlAx7oo8hWBoSN8dtn5l7zuMeQm6NcKt2QEkmI-KK8nBT2nFj6ozRpdy7Jah74u3y7XiSk_avYA-IQ97EsuEeg=='
+
+nyt_api_key = "phjICRQDF6AY2FWxi8DGnN6rAtt3rcFg"
+
+census_api_key = "4988bdee228568b0e285351bd78eb88311595fab"
